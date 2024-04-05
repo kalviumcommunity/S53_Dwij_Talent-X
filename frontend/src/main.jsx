5abb023c99bbcaecf723b5d481518cad3bd051ad
Dwij13 <dwijjindal2005@gmail.com>
2024-04-05 09:52:08 +0530

    Added check for publishable_key

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,9 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import { BrowserRouter } from "react-router-dom";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
