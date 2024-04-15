7f3aba4a7b52b78f7b96bb692d641f63e367bff2
Dwij13 <dwijjindal2005@gmail.com>
2024-04-15 13:53:08 +0530

    Synced clerk user data with mongoDb

--- frontend/src/Components/SignIn.jsx
+++ frontend/src/Components/SignIn.jsx
@@ -1,8 +1,39 @@
-import React from "react";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
- 
+import React, { useEffect, useState } from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useSession,
+  useUser,
+} from "@clerk/clerk-react";
+import axios from "axios";
 
 export default function SignIn() {
+  const { user } = useUser();
+  useEffect(() => {
+    const signIn = async () => {
+      try {
+        console.log(user);
+        let userName = user.username == null ? user.fullName : user.username;
+        let userEmail = user.emailAddresses[0].emailAddress;
+        const userdata = {
+          userId: user.id,
+          name: userName,
+          email: userEmail,
+        };
+        console.log(userdata);
+        await axios.post("http://localhost:6996/users", userdata);
+      } catch (error) {
+        console.log("Please sign in or check your network connection.");
+      }
+    };
+
+    if (user) {
+      signIn();
+    }
+  }, [user]);
+
   return (
     <header>
       <div>H1</div>
